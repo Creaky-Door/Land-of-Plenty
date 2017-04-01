61fd8102d3ed476dab45966b294a1730a0715a9e
Ryan Lee <rlee33@gmail.com>
2017-04-01 16:30:40 -0500

    init
    Initial commit
    cleanup

---
+++ Land of Plenty/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  Land of Plenty
+//
+//  Created by Ryan Lee on 4/1/17.
+//  Copyright © 2017 Creaky-Door. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
